a98054b573faeb40e3371cb30327b276fce42474
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2025-03-12 15:58:40 -0300

    chore: update rules to use main stylistic package

--- index.js
+++ index.js
@@ -1,70 +1,71 @@
 import pluginJs from "@eslint/js";
-import globals from "globals";
 import tseslint from "typescript-eslint";
+import stylistic from '@stylistic/eslint-plugin'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    languageOptions: { globals: globals.browser },
-
-    ignorePatterns: ["dist/"],
+    plugins: {
+      '@stylistic': stylistic
+    },
 
     rules: {
-      semi: "error",
-      "arrow-body-style": ["error", "as-needed"],
+      semi: ['error', 'never'],
       camelcase: [
-        "warn",
+        'warn',
         {
-          properties: true,
+          properties: 'always',
           ignoreDestructuring: true,
           ignoreImports: true,
           ignoreGlobals: false,
         },
       ],
-      "capitalized-comments": "warn",
-      curly: "error",
-      "default-case-last": "error",
-      "dot-notation": "error",
-      "func-style": [
-        "error",
-        "declaration",
+      'capitalized-comments': 'warn',
+      curly: 'error',
+      'default-case-last': 'error',
+      'dot-notation': 'error',
+      'func-style': [
+        'error',
+        'declaration',
         {
           allowArrowFunctions: true,
-          overrides: { namedExports: "declaration" },
+          overrides: { namedExports: 'declaration' },
         },
       ],
-      "no-alert": "error",
-      "no-console": ["error", { allow: ["warn", "error"] }],
-      "no-else-return": "error",
-      "no-empty": "error",
-      "no-empty-function": "error",
-      "no-eval": "error",
-      "no-extra-boolean-cast": "error",
-      "no-lone-blocks": "error",
-      "no-magic-numbers": [
-        "warn",
+      'no-alert': 'error',
+      'no-console': ['error', { allow: ['warn', 'error'] }],
+      'no-else-return': 'error',
+      'no-empty': 'error',
+      'no-empty-function': 'error',
+      'no-eval': 'error',
+      'no-extra-boolean-cast': 'error',
+      'no-lone-blocks': 'error',
+      'no-magic-numbers': [
+        'warn',
         {
           ignore: [-1, 0, 1, 2, 10, 100],
           ignoreArrayIndexes: true,
         },
       ],
-      "@stylistic/js/arrow-parens": ["error", "always"],
-      "@stylistic/js/arrow-spacing": "error",
-      "@stylistic/js/eol-last": ["error", "always"],
-      "@stylistic/js/no-multiple-empty-lines": [
-        "error",
+      '@stylistic/arrow-parens': ['error', 'always'],
+      '@stylistic/arrow-spacing': 'error',
+      '@stylistic/eol-last': ['error', 'always'],
+      '@stylistic/no-multiple-empty-lines': [
+        'error',
         { max: 2, maxBOF: 0, maxEOF: 1 },
       ],
-      "@stylistic/js/no-multi-spaces": "error",
-      "@stylistic/js/quotes": [
-        "warn",
-        "single",
+      '@stylistic/no-multi-spaces': 'error',
+      '@stylistic/quotes': [
+        'warn',
+        'single',
         { avoidEscape: true, allowTemplateLiterals: true },
       ],
-      "no-var": "warn",
-      "prefer-exponentiation-operator": "warn",
-      yoda: "error",
+      'no-var': 'warn',
+      'prefer-exponentiation-operator': 'warn',
+      yoda: 'error',
     },
   },
+  eslintPluginPrettierRecommended,
 ];
